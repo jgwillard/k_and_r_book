64e868adb41bc1ba00a1f3e955805429841e15e6
John Willard <willard.john.gray@gmail.com>
2023-10-18 21:38:46 -0400

    add gitignore, add explanatory comments

--- 1/06_check_getchar_equals_eof.c
+++ 1/06_check_getchar_equals_eof.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * Verify that that the expression `getchar() != EOF` is either 0 or 1.
+ *
+ * Accepts input and print the value of the above expression. When
+ * expression is 0, exit loop. Will have the effect of printing 1 for
+ * every character of input until EOF, when it will print 0 and stop.
+ */
 int main() {
     int c;
 
--- 1/07_print_value_of_eof.c
+++ 1/07_print_value_of_eof.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/**
+ * Print the value of EOF.
+ */
 int main() {
     printf("%d\n", EOF);
 }
