8cb7bcd08debb5977328589796c661b90646d8d0
John Willard <willard.john.gray@gmail.com>
2023-10-21 23:53:08 -0400

    print histogram vertically for 1-13

--- 1/13_word_length_histogram.c
+++ 1/13_word_length_histogram.c
@@ -4,12 +4,36 @@
 #define OUT 0
 #define IN 1
 
+/**
+ * Print a histogram of the lengths of words in input.
+ *
+ * Program consists of three parts: initialization, reading input, and
+ * printing the histogram.
+ *
+ * Initialization creates the histogram array and sets all values to 0.
+ *
+ * Reading input is similar to the previous exercise: we use a state
+ * variable to track whether we are in a word so that we know when we
+ * enter and leave a new word. When we enter a word we reset the length
+ * counter to 1 and when we leave a word we record the length of the
+ * word in the histogram. We also track the most frequently occurring
+ * length so that we know the max height of the histogram.
+ *
+ * To print the histogram we start with the max height and count down,
+ * and for each column we print a `#` if the value in the histogram for
+ * that column is greater than or equal to the height counter.
+ *
+ * NOTE: a "word" in this program is any string of non-blank
+ * characters, so e.g. `histogram[i]` is treated as a single word.
+ */
 int main() {
+    // init
     int i;
     int c;
     int state;
     int is_wordbreak;
     int word_length = 0;
+    int max_frequency = 0;
     int histogram[HISTOGRAM_LENGTH];
     int j;
 
@@ -17,12 +41,13 @@ int main() {
         histogram[i] = 0;
     }
 
+    // read input
     state = OUT;
     while ((c = getchar()) != EOF) {
         is_wordbreak = (c == ' ' || c == '\n' || c == '\t');
-        // we have entered a word so start counting or
+        // we have entered a word so start counting the first char
         if (!is_wordbreak && state == OUT) {
-            word_length += 1;
+            word_length = 1;
             state = IN;
         }
         // we are in a word so continue counting
@@ -33,26 +58,32 @@ int main() {
         else if (is_wordbreak && state == IN) {
             state = OUT;
             if (word_length < HISTOGRAM_LENGTH) {
-                histogram[word_length - 1] += 1;
+                i = word_length - 1;
             }
             else {
-                histogram[HISTOGRAM_LENGTH - 1] += 1;
+                i = HISTOGRAM_LENGTH - 1;
             }
-            word_length = 0;
+            histogram[i] += 1;
+            if (histogram[i] > max_frequency) {
+                max_frequency = histogram[i];
+            }
         }
     }
 
     // print histogram
-    for (i = 0; i < HISTOGRAM_LENGTH - 1; i++) {
-        printf("Word length %d:", i + 1);
-        for (j = 0; j < histogram[i]; j++) {
-            putchar('#');
+    for (i = max_frequency; i > 0; i--) {
+        for (j = 0; j < HISTOGRAM_LENGTH; j++) {
+            if (histogram[j] >= i) {
+                printf("  #");
+            }
+            else {
+                printf("   ");
+            }
         }
         putchar('\n');
     }
-    printf("Word length %d+:", HISTOGRAM_LENGTH);
-    for (i = 0; i < histogram[HISTOGRAM_LENGTH - 1]; i++) {
-        putchar('#');
+    for (i = 0; i < HISTOGRAM_LENGTH - 1; i++) {
+        printf("%3d", i + 1);
     }
-    putchar('\n');
+    printf("%3d+\n", HISTOGRAM_LENGTH);
 }
