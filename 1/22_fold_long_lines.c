078d0c9a60555ac362e3ef8d2a2b32bef03b4e30
John Willard <willard.john.gray@gmail.com>
2024-01-09 23:07:06 -0500

    fold lines working with test.txt

--- 1/22_fold_long_lines.c
+++ 1/22_fold_long_lines.c
@@ -1,82 +1,89 @@
 #include <stdio.h>
 
-#define MAXLENGTH 1000
 #define FOLDLENGTH 59
 #define IN 1
 #define OUT 0
 
-/**
- * Read input and copy characters into array s until newline or limit
- * is reached. If a line exceeds FOLDLENGTH, break after the last non-
- * blank character before FOLDLENGTH. Return length of line.
- */
-int get_folded_line(char line[], int maxline);
+void print_folded_line(char line[], int count);
 
 /**
- * Read line be line. If line length exceeds FOLDLENGTH, print on
- * multiple lines.
+ * Fold long input lines into two or more shorter lines after the last
+ * non-blank character that occurs before the n-th column of input.
  */
-int main() {
-    char current_line[MAXLENGTH];
-
-    while (get_folded_line(current_line, MAXLENGTH)) {
-        printf("%s", current_line);
-    }
+int main()
+{
+    char buffer[FOLDLENGTH];
+    print_folded_line(buffer, 0);
 }
 
-int get_folded_line(char s[], int lim) {
-    int state;
+/**
+ * Read input and print in a line until newline or FOLDLENGTH is
+ * reached. If FOLDLENGTH is reached before newline, print everything up
+ * to the current word on its own line, then print the current word and
+ * the rest of the line on the next line.
+ *
+ * (base case)
+ * When you reach a newline, reset character count, then print buffer.
+ *
+ * (recursive case)
+ * If count reaches limit, reset count then back current buffer up to
+ * beginning of current word. Create a new buffer and add current word
+ * to it. Then call function with the new buffer and current index.
+ */
+void print_folded_line(char buffer[], int count)
+{
     int i;
     int c;
+    int state;
     int is_whitespace;
     int is_newline;
-    int end_last_word_index = 0;
+    int last_word_index;
+    char new_buffer[FOLDLENGTH];
 
     state = OUT;
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF; i++) {
+    while ((c = getchar()) != EOF)
+    {
         is_whitespace = (c == ' ' || c == '\t');
         is_newline = c == '\n';
+        buffer[count] = c;
+        count++;
 
-        // copy current character to new string
-        s[i] = c;
-
-        // we are inside a word and encounter whitespace
-        if (state == IN && is_whitespace) {
-            end_last_word_index = i;
+        // set states
+        //
+        // leaving word
+        if (state == IN && is_whitespace)
+        {
+            last_word_index = count;
             state = OUT;
         }
-        // we are outside a word and encounter non-whitespace, non-
-        // newline characters
-        else if (state == OUT && !is_whitespace && !is_newline) {
+
+        // entering word
+        else if (state == OUT && !is_whitespace && !is_newline)
+        {
             state = IN;
         }
-        // we are inside or outside a word and encounter a newline
-        else if (is_newline) {
-            // save null to next index and leave loop
-            s[++i] = '\0';
-            break;
-        }
-        // we reach FOLDLENGTH without having reached a newline and we
-        // are outside of a word
-        else if (i == FOLDLENGTH && state == OUT) {
-            s[i] = '\n';
-            s[++i] = '\0';
-            break;
-        }
-        // we reach FOLDLENGTH without having reached a newline and we
-        // are in a word
-        else if (i == FOLDLENGTH && state == IN) {
-            // go back to the last index where there was a word
-            i = end_last_word_index;
-            // now we have to go back and prepend the characters
-            // between the previous value of i and the value of
-            // end_last_word_index to the next line...
-            s[i] = '\n';
-            s[++i] = '\0';
-            break;
+
+        // base case
+        if (is_newline)
+        {
+            buffer[count] = '\0';
+            count = 0;
+            printf("%s", buffer);
         }
 
+        // recursive case
+        else if (count > FOLDLENGTH)
+        {
+            // copy current word to new buffer
+            for (i = 0; i < count - last_word_index; i++)
+            {
+                new_buffer[i] = buffer[last_word_index + i];
+            }
+            // terminate current buffer
+            buffer[last_word_index - 1] = '\n';
+            buffer[last_word_index] = '\0';
+            printf("%s", buffer);
+            print_folded_line(new_buffer, count - last_word_index);
+        }
     }
-
-    return i;
 }
