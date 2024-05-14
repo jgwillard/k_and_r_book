4dae2c081b61c342d155c601898aef1ad68d3e19
John Willard <willard.john.gray@gmail.com>
2024-05-13 22:09:05 -0400

    use macros to reduce code repetition

--- 2/1_determine_ranges.c
+++ 2/1_determine_ranges.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
 #include <limits.h>
 
-int get_min_char()
-{
-    return ~(char)((unsigned char)~0 >> 1);
-}
+#define MIN_MAX_FUNCS(type)                \
+    int get_min_##type()                   \
+    {                                      \
+        return ~((unsigned type) ~0 >> 1); \
+    }                                      \
+    int get_max_##type()                   \
+    {                                      \
+        return ((unsigned type) ~0 >> 1);  \
+    }                                      \
+    int get_min_unsigned_##type()          \
+    {                                      \
+        return (unsigned type)0;           \
+    }                                      \
+    int get_max_unsigned_##type()          \
+    {                                      \
+        return (unsigned type)(~0);        \
+    }
 
-int get_max_char()
-{
-    return (char)((unsigned char)~0 >> 1);
-}
+#define PRINT_MIN_MAX(type)                                                       \
+    printf("Range of " #type ": %d -> %d\n", get_min_##type(), get_max_##type()); \
+    printf("Range of unsigned " #type ": %u -> %u\n", get_min_unsigned_##type(), get_max_unsigned_##type());
 
-int get_min_unsigned_char()
-{
-    return (unsigned char)0;
-}
-
-int get_max_unsigned_char()
-{
-    return (unsigned char)(~0);
-}
+MIN_MAX_FUNCS(char)
+MIN_MAX_FUNCS(short)
+MIN_MAX_FUNCS(int)
 
 int main()
 {
-    printf("Range of char: %d - %d\n", get_min_char(), get_max_char());
-    printf("Range of unsigned char: %d - %d\n", get_min_unsigned_char(), get_max_unsigned_char());
+    PRINT_MIN_MAX(char)
+    PRINT_MIN_MAX(short)
+    PRINT_MIN_MAX(int)
+    // FIXME macros do not work for longs because (1) the return type
+    // must be long not int and (2) the format specifiers must be
+    // %ld/%lu not %d/%u
+    printf("Range of long: %ld -> %ld\n", ~((unsigned long)~0 >> 1), ((unsigned long)~0 >> 1));
+    printf("Range of unsigned long: %lu -> %lu\n", (unsigned long)0, (unsigned long)~0);
 }
