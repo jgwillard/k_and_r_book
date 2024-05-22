ff2581b8898da6297106bf32bc1f025b2d3109f2
John Willard <willard.john.gray@gmail.com>
2024-05-21 22:53:57 -0400

    test and document rightrot (ex 2-8)

--- 2/08_rightrot.c
+++ 2/08_rightrot.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * Gets the size of an integer in the system by calculating the base-2
- * logarithm of the largest possible integer.
+ * Gets the size in bits of an integer in the system by calculating the
+ * base-2 logarithm of the largest possible integer.
  */
 int int_size()
 {
@@ -17,13 +17,16 @@ int int_size()
 }
 
 /**
- * TODO test and document
+ * Returns the value of integer `x` rotated to the right by `n`
+ * positions.
  */
 unsigned rightrot(unsigned x, int n)
 {
+    // get number of bits in an integer
     int size = int_size();
     // bitmask of rightmost n bits
     unsigned mask = ~(~0 << n);
+    // get the rightmost bits in x
     unsigned right_bits = mask & x;
     // shift rightmost n bits to leftmost position
     unsigned left_bits = right_bits << (size - n);
@@ -35,5 +38,8 @@ unsigned rightrot(unsigned x, int n)
 
 int main()
 {
-    printf("0x%x\n", rightrot(1, 1));
+    printf("0x%x\n", rightrot(0x1, 1)); // -> 0x80000000
+    printf("0x%x\n", rightrot(0x3, 2)); // -> 0xc0000000
+    printf("0x%x\n", rightrot(0x2, 1)); // -> 0x00000001
+    printf("0x%x\n", rightrot(0xe, 4)); // -> 0xe0000000
 }
