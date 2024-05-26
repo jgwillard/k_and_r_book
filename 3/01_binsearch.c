60d445667706d2dc91c28d232d1539db0a6c9a43
John Willard <willard.john.gray@gmail.com>
2024-05-25 20:43:01 -0400

    implement alt binsearch function and time both

--- 3/01_binsearch.c
+++ 3/01_binsearch.c
@@ -49,6 +49,33 @@ int binsearch(int x, int v[], int n)
     return -1;
 }
 
+int alt_binsearch(int x, int v[], int n)
+{
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+    while (low < high)
+    {
+        mid = (low + high) / 2;
+        if (x <= v[mid])
+        {
+            high = mid;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+    if (x == v[mid])
+    {
+        return mid;
+    }
+
+    return -1;
+}
+
 int main()
 {
     // seed random number generator once at the beginning of the program
@@ -59,16 +86,51 @@ int main()
     int i;
     int target;
     int found;
+    struct timespec start, end;
+    long seconds;
+    long nanoseconds;
 
     rand_sorted_array(array, n);
 
-    // Print the sorted array
-    for (i = 0; i < n * 100; i++)
+    // start timer
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    for (i = 0; i < n * 10000; i++)
     {
         target = rand();
         found = binsearch(target, array, n);
-        printf("Found %d\n", found);
+        if (found != -1)
+        {
+            printf("Found %d\n", array[found]);
+        }
     }
+
+    // stop timer
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    seconds = end.tv_sec - start.tv_sec;
+    nanoseconds = end.tv_nsec - start.tv_nsec;
+    printf("Elapsed time for binsearch:     %ld seconds %ld nanoseconds\n", seconds, nanoseconds);
+
+    // start timer
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    for (i = 0; i < n * 10000; i++)
+    {
+        target = rand();
+        found = alt_binsearch(target, array, n);
+        if (found != -1)
+        {
+            printf("Found %d\n", array[found]);
+        }
+    }
+
+    // stop timer
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    seconds = end.tv_sec - start.tv_sec;
+    nanoseconds = end.tv_nsec - start.tv_nsec;
+    printf("Elapsed time for alt_binsearch: %ld seconds %ld nanoseconds\n", seconds, nanoseconds);
 
     return 0;
 }
